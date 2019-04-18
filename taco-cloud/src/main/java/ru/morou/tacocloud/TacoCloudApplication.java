c943e1e7677b0b029aef4d70557cf889798306f2
Mori <mori_morou@rocketmail.com>
2019-04-18 23:01:42 +0300

    added

--- taco-cloud/src/main/java/ru/morou/tacocloud/TacoCloudApplication.java
+++ taco-cloud/src/main/java/ru/morou/tacocloud/TacoCloudApplication.java
@@ -3,11 +3,19 @@ package ru.morou.tacocloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Аннотация @SpringBootApplication ясно показывает, что это приложение Spring Boot.
+ * @SpringBootApplication - составное приложение, которое объединяет три другие аннотации:
+ * @SpringBootConfiguration - обозначает этот класс как класс конфигурации.
+ * @EnableAutoConfiguration - включает автоматическую настройку Spring Boot.
+ * @ComponentScan - включает сканирование компонентов. Это позволяет вам объявить другие
+ * классы с аннотациями
+ */
+
 @SpringBootApplication
 public class TacoCloudApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TacoCloudApplication.class, args);
     }
-
 }
