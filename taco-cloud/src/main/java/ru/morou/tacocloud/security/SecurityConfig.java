2fb2e8ce3fc6937864940d970a65b1ca4789d3f1
MoriMorou <Mori_morou@rocketmail.com>
2019-04-25 23:44:13 +0300

    added

---
+++ taco-cloud/src/main/java/ru/morou/tacocloud/security/SecurityConfig.java
@@ -0,0 +1,20 @@
+package ru.morou.tacocloud.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * настройка Spring Security
+ */
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth)
+            throws Exception {
+    }
+}
