1706ab89ce4dabc35ff4b3a2c703e292d7277df3
Mori <mori_morou@rocketmail.com>
2019-05-06 00:50:29 +0300

    added mvs

--- LessonTwo/src/main/java/ru/morou/controllers/StudentsController.java
+++ LessonTwo/src/main/java/ru/morou/controllers/StudentsController.java
@@ -24,4 +24,39 @@ public class StudentsController {
         System.out.println(student.getFirstName() + " " + student.getLastName());
         return "students";
     }
+
+    // запрос через ? знак (/showStudentByID?id=7
+//    @RequestMapping(path = "/showStudentByID", method = RequestMethod.GET)
+//    @ResponseBody
+//    public Student showStudentByID(Model uiModel, @RequestParam int id) {
+//        Student student = studentsService.getStudentByID(id);
+//        return student;
+//    }
+
+    // запрос через / (то есть /showStudentByID/7)
+    @RequestMapping(path = "/showStudentByID/{sid}", method = RequestMethod.GET)
+    @ResponseBody
+    public Student showStudentById(@PathVariable("sid") int id) {
+        Student student = studentsService.getStudentByID(id);
+        return student;
+    }
+
+    // Отличия RequestMethod.GET от RequestMethod.POST в том что в get передача id, а в post передача объекта
+    // Например для регистрации или авторизации
+    @RequestMapping(path = "/setStudentByID", method = RequestMethod.POST)
+    public void setStudent(@RequestBody Student student) {
+        System.out.println(student.getFirstName() + " " + student.getLastName());
+    }
+
+    @RequestMapping("/showForm")
+    public String showSimpleForm(Model uiModel) {
+        Student student = new Student();
+        uiModel.addAttribute("student", student);
+        return "student-form";
+    }
+
+    @RequestMapping("/processForm")
+    public String processForm(@ModelAttribute("student") Student student) {
+        return "student-form-result";
+    }
 }
