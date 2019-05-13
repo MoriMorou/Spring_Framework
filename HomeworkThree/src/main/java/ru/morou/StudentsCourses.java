df4fb93549a74e25e577a16713ccd10e41d3d47a
Mori <mori_morou@rocketmail.com>
2019-05-13 12:17:29 +0300

    added

---
+++ HomeworkThree/src/main/java/ru/morou/StudentsCourses.java
@@ -0,0 +1,43 @@
+package ru.morou;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "students_courses")
+public class StudentsCourses {
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "marks")
+    private int marks;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getMarks() {
+        return marks;
+    }
+
+    public void setMarks(int marks) {
+        this.marks = marks;
+    }
+
+    public StudentsCourses() {
+    }
+
+    @Override
+    public String toString() {
+        return "StudentsCourses{" +
+                "id=" + id +
+                ", marks=" + marks +
+                '}';
+    }
+}
