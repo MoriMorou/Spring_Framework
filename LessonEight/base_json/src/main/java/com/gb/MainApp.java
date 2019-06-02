8b7381ad96c22c14af1ccb778f64081dec058330
MoriMorou <Mori_morou@rocketmail.com>
2019-06-02 17:49:20 +0300

    added

--- LessonEight/base_json/src/main/java/com/gb/MainApp.java
+++ LessonEight/base_json/src/main/java/com/gb/MainApp.java
@@ -8,14 +8,14 @@ import java.io.IOException;
 
 public class MainApp {
     public static void main(String[] args) throws IOException {
-        Student student = new Student(1, "Bob");
+//        Student student = new Student(1, "Bob");
+//
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+//        objectMapper.writeValue(new File("student.json"), student);
 
         ObjectMapper objectMapper = new ObjectMapper();
-//        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
-        objectMapper.writeValue(new File("student.json"), student);
-
-//        ObjectMapper objectMapper = new ObjectMapper();
-//        Student student = objectMapper.readValue(new File("student.json"), Student.class);
-//        student.info();
+        Student student = objectMapper.readValue(new File("student.json"), Student.class);
+        student.info();
     }
 }
