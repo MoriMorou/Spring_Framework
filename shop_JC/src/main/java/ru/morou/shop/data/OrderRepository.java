b626bc3580cfc038027e2f3e9a5fbca54d2e4afa
Mori <mori_morou@rocketmail.com>
2019-06-17 18:08:44 +0300

    added

--- shop_JC/src/main/java/ru/morou/shop/data/OrderRepository.java
+++ shop_JC/src/main/java/ru/morou/shop/data/OrderRepository.java
@@ -1,20 +1,20 @@
-package ru.morou.shop.data;
-
-import org.springframework.data.repository.CrudRepository;
-import ru.morou.shop.entities.Order;
-
-public interface OrderRepository extends CrudRepository<Order, Long> {
-
-//    List<Order> findByDeliveryZip(String deliveryZip);
-
-//    List<Order> readOrdersByDeliveryZipAndPlacedAtBetween(
-//            String deliveryZip, Date startDate, Date endDate);
-
-
-//    List <Order> findByDeliveryToAndDeliveryCityAllIgnoresCase (
-//            String deliveryTo, String deliveryCity);
-
-
-//    @Query("Order o where o.deliveryCity='Seattle'")
-//    List<Order> readOrdersDeliveredInSeattle();
-}
+//package ru.morou.shop.data;
+//
+//import org.springframework.data.repository.CrudRepository;
+//import ru.morou.shop.entities.Order;
+//
+//public interface OrderRepository extends CrudRepository<Order, Long> {
+//
+////    List<Order> findByDeliveryZip(String deliveryZip);
+//
+////    List<Order> readOrdersByDeliveryZipAndPlacedAtBetween(
+////            String deliveryZip, Date startDate, Date endDate);
+//
+//
+////    List <Order> findByDeliveryToAndDeliveryCityAllIgnoresCase (
+////            String deliveryTo, String deliveryCity);
+//
+//
+////    @Query("Order o where o.deliveryCity='Seattle'")
+////    List<Order> readOrdersDeliveredInSeattle();
+//}
--- shop_JC/src/main/java/ru/morou/shop/data/ShopingCartRepository.java
+++ shop_JC/src/main/java/ru/morou/shop/data/ShopingCartRepository.java
@@ -1,7 +1,7 @@
-package ru.morou.shop.data;
-
-import org.springframework.data.repository.CrudRepository;
-import ru.morou.shop.entities.ShopingCart;
-
-public interface ShopingCartRepository extends CrudRepository<ShopingCart, Long> {
-}
+//package ru.morou.shop.data;
+//
+//import org.springframework.data.repository.CrudRepository;
+//import ru.morou.shop.entities.ShopingCart;
+//
+//public interface ShopingCartRepository extends CrudRepository<ShopingCart, Long> {
+//}
