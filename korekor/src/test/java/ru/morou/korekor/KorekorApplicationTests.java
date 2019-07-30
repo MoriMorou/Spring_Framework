6b858a98a9b8102fc74ced6788604eff7654589c
Mori <mori_morou@rocketmail.com>
2019-07-30 16:36:51 +0300

    Не работает с 2h, с нормальной базой получается

--- korekor/src/test/java/ru/morou/korekor/KorekorApplicationTests.java
+++ korekor/src/test/java/ru/morou/korekor/KorekorApplicationTests.java
@@ -2,15 +2,34 @@ package ru.morou.korekor;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class KorekorApplicationTests {
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
 
     @Test
-    public void contextLoads() {
+    public void testUserRestController() {
+        ResponseEntity<String[]> entity = restTemplate.getForEntity(String.format("http://localhost:%d/api/users", port), String[].class);
+        assertEquals(HttpStatus.OK, entity.getStatusCode());
+        assertNotNull(entity.getBody());
+        assertEquals(1, entity.getBody().length);
+        assertEquals("admin", entity.getBody()[0]);
     }
-
 }
