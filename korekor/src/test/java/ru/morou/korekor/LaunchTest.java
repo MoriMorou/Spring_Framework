9289b227f796f4811484e4bfe7774957a3e0f713
Mori Morou <mori_morou@yahoo.com>
2019-08-04 21:02:01 +0300

    tests

---
+++ korekor/src/test/java/ru/morou/korekor/LaunchTest.java
@@ -0,0 +1,15 @@
+package ru.morou.korekor;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"html:target/cucumber-html-report"},
+        features = {"classpath:features"},
+        glue = {"ru.morou.korekor.steps"},
+        snippets = SnippetType.CAMELCASE)
+public class LaunchTest {
+
+}
